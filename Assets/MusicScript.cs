2dd14834aa5c908fd805f78b675ee7c931dac377
vallient <riera.valentin2a@gmail.com>
2019-01-03 17:56:17 +0100

    Update project to unity 2018.3
    update project to unity 2018.3 and add music gameobject into menu scene

---
+++ Assets/MusicScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicScript : MonoBehaviour {
+
+    static MusicScript instance;
+
+	// Use this for initialization
+	void Awake () {
+	    if(instance != null)
+        {
+            Destroy(gameObject);
+        }else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
